71530534faf9d749d861219ce26da28338a96051
Bahseet-altschool <bahseet2022@gmail.com>
2025-05-04 18:11:28 +0100

    Update RevenueChart.jsx

--- Frontend/src/components/DashboardComponents/RevenueChart.jsx
+++ Frontend/src/components/DashboardComponents/RevenueChart.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Line } from "react-chartjs-2";
 import { Chart as ChartJS, defaults } from "chart.js/auto";
 
-import  SourceData from "../data/SourceData.json";
+import  SourceData from "../data/sourcedata.json";
 import styles from "../../styles/dashboard.module.css";
 
 defaults.maintainAspectRatio = false;
